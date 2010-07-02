c843f74fa7d6af437ea2a4e057f1bac4b5a30897
Dale Jung <dale@dalejung.com>
2010-07-02 12:31:00 -0400

    added panay theme to test panels everywhere

---
+++ sites/all/themes/contrib/panany/plugins/styles/panany_default/panany-default-box.tpl.php
@@ -0,0 +1,12 @@
+<?php
+// $Id: panany-default-box.tpl.php,v 1.1 2010/01/11 05:35:13 jwolf Exp $
+
+/**
+ *
+ * Markup for blocks.
+ *
+ */
+?>
+<div class="panany">
+  <?php print $content; ?>
+</div><!-- /panany -->
---
+++ sites/all/themes/contrib/panany/plugins/styles/panany_gradient/panany-gradient-box.tpl.php
@@ -0,0 +1,12 @@
+<?php
+// $Id: panany-gradient-box.tpl.php,v 1.1 2010/01/11 05:35:13 jwolf Exp $
+
+/**
+ *
+ * Markup for blocks.
+ *
+ */
+?>
+<div class="panany-gradient">
+  <?php print $content; ?>
+</div><!-- /panany -->
