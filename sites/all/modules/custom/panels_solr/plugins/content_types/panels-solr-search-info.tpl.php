1792d75264eb638bd5d87acb09e74f319b60d875
Dale Jung <dale@dalejung.com>
2010-07-03 15:07:12 -0400

    ADD: panels_solr DESC: Module to make multiple solr search pages with panels

---
+++ sites/all/modules/custom/panels_solr/plugins/content_types/panels-solr-search-info.tpl.php
@@ -0,0 +1,16 @@
+<?php
+ // Expose as much as I can via preprocess
+ // This should overriden 
+?>
+
+<?php if(isset($result_count)): ?>
+  <?php print $start; ?> of <?php print $end; ?>
+<?php endif; ?>
+
+<?php if(isset($result_count)): ?>
+  <?php print $result_count; ?>
+<?php endif; ?>
+
+<?php if($search_terms): ?>
+  <?php print $search_terms; ?>
+<?php endif; ?>
